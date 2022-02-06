269cd03b962d77e0eacf78e6bd98b5a2ccaf8415
elvisfernandess <94504194+elvisfernandess@users.noreply.github.com>
2022-02-06 19:43:33 -0300

    Add files via upload

---
+++ apirest/src/main/java/com/temperatura/apirest/resources/TemperaturaResources.java
@@ -0,0 +1,25 @@
+package com.temperatura.apirest.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.temperatura.apirest.models.Temperatura;
+import com.temperatura.apirest.repository.TemperaturaRepository;
+
+@RestController
+@RequestMapping(value = "/api")
+public class TemperaturaResources {
+
+	@Autowired
+	TemperaturaRepository temperaturaRepository;
+
+	@GetMapping("/temperatura")
+
+	public List <Temperatura> listaTemperaturas() {
+		return temperaturaRepository.findAll();
+	}
+}
