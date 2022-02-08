babc14f4a25d305f5ccb9ed0205f3e070c6f2380
elvisfernandess <94504194+elvisfernandess@users.noreply.github.com>
2022-02-07 21:58:42 -0300

    Add files via upload

--- apirest/src/main/java/com/temperatura/apirest/resources/TemperaturaResources.java
+++ apirest/src/main/java/com/temperatura/apirest/resources/TemperaturaResources.java
@@ -2,8 +2,12 @@ package com.temperatura.apirest.resources;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,6 +31,23 @@ public class TemperaturaResources {
 	public Object listaTemperaturaUnica(@PathVariable(value = "id") long id) {
 		return temperaturaRepository.findById(id);
 	}
+	
+	@PostMapping("/temperatura/salvar")
+	public Temperatura salvaProduto(@RequestBody Temperatura temperatura) {
+		return temperaturaRepository.save(temperatura);
+	}
+	
+	@DeleteMapping("/temperatura/deletar")
+	public void deletaTemperatura(@RequestBody Temperatura temperatura) {
+		temperaturaRepository.delete(temperatura);
+	}
+	
+	@PutMapping("/temperatura/atualizar")
+	public Temperatura atualizaTemperatura(@RequestBody Temperatura temperatura) {
+			return temperaturaRepository.save(temperatura);
+	}
+
+		
 }
 
 
